2007fb3559dc7d6164b695ade70de38500a225cb
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-09-23 14:09:10 +0200

    Update coreutils.py

--- tor2tor/coreutils.py
+++ tor2tor/coreutils.py
@@ -1,8 +1,12 @@
 import os
+import time
 import subprocess
+from datetime import datetime
 from urllib.parse import urlparse
 
+from PIL import Image
 from rich import print
+
 from .config import load_data
 
 # Construct path to the user's home directory
@@ -31,7 +35,57 @@ def path_finder(url: str):
         # Construct and create each directory from the directories list if it doesn't already exist
         os.makedirs(os.path.join(HOME_DIRECTORY, directory), exist_ok=True)
 
+def format_time_difference(current_time: float, past_time: float) -> string:
+    """
+    Format the time difference between the current time and a past time.
+    
+    :param current_time: (float) The current time as a Unix timestamp.
+    :param past_time: (float) The past time as a Unix timestamp.
+    :return: A string representing the time difference in a human-readable format.
+    """
+    
+    # Calculate the time difference in seconds
+    time_difference = int(current_time - past_time)
+    
+    # If the time difference is less than 1 second, return "now"
+    if time_difference < 1:
+        return "now"
+    
+    # If the time difference is less than 60 seconds, return in seconds
+    if time_difference < 60:
+        return f"{time_difference} seconds ago"
+    
+    # Convert time difference to minutes and check if less than 60 minutes
+    minutes = time_difference // 60
+    if minutes < 60:
+        return f"{minutes} minutes ago"
+    
+    # Convert time difference to hours and check if less than 24 hours
+    hours = minutes // 60
+    if hours < 24:
+        return f"{hours} hours ago"
 
+    # For times older than a day, return the exact date and time
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(past_time))
+    
+    
+def get_file_info(filename: str) -> tuple:
+  """
+  Gets a given file's information.
+  
+  :param filename: File to get info for.
+  :return: A tuple containing the file's dimensions, size and last modified time.
+  """
+  dimensions = None
+  with Image.open(filename) as image:
+      dimensions = image.size
+      
+  file_size = os.path.getsize(filename)
+  last_modified_time = format_time_difference(current_time=time.time(), past_time=os.path.getmtime(filename))
+  
+  return dimensions, file_size, last_modified_time
+  
+  
 def clear_screen():  # -> a cleared screen
     """
     Clear the terminal screen/
