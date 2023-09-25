498395ef476190f0abe01b11ff6f7c954cebee5c
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-09-25 11:13:28 +0200

    Add files via upload
    Install and Uninstall shell scripts

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+apt-get update && apt-get install -y --no-install-recommends tor
+
+curl -L https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz | \
+    tar xz -C /usr/local/bin
+
+pip3 install .
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+rm /usr/local/bin/geckodriver -v
+apt remove tor --autoremove --purge -y
+pip3 uninstall tor2tor -y -v
