c37454f95db0ecc4425b5b7caa07f0c8b9a9510b
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-09-26 04:18:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 apt-get update && apt-get install -y --no-install-recommends tor
 
 curl -L https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz | \
-    tar xz -C /bin
+    tar xz -C /usr/bin
 
 pip3 install .
