f0e16758e107f39f53588196038a40f0325fd45f
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-09-27 22:03:52 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
+# Remove the geckodriver binary from /usr/bin
 rm /usr/bin/geckodriver -v
+
+# Uninstall tor and its configuration files including unused packages (I might be doing you a favour by removing unused packages haha)
 apt remove tor --autoremove --purge -y
+
+# Uninstall tor2tor
 pip3 uninstall tor2tor -y -v
