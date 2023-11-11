c33cf1b129907de7a7f767c1775e8898f78d3f3e
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-11-11 23:25:05 +0200

    Update __init__.py

--- tor2tor/__init__.py
+++ tor2tor/__init__.py
@@ -1,3 +1,20 @@
 __author__ = "Richard Mwewa"
 __about__ = "https://about.me/rly0nheart"
 __version__ = "0.12.0"
+__description__ = """
+# Tor2Tor
+> **Tor2Tor** scrapes a given onion link and captures screenshots of all links available on it.
+"""
+
+__epilog__ = f"""
+# by [{__author__}]({__about__})
+## Examples
+### Local Installation
+```
+tor2tor http://example.onion
+```
+
+### Docker Container
+```
+docker run --tty --volume $PWD/tor2tor:/root/tor2tor rly0nheart/tor2tor http://example.onion
+```"""
